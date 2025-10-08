e69a7a85f4418a8d1fae125357e02cd50d8a421f
FMagalhaes25 <felipemostaco05@gmail.com>
2025-10-08 10:44:11 -0300

    First commit

---
+++ src/main/java/pi2025_semestre6/metro_obras/MetroObrasApplication.java
@@ -0,0 +1,13 @@
+package pi2025_semestre6.metro_obras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroObrasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetroObrasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pi2025_semestre6/metro_obras/MetroObrasApplicationTests.java
@@ -0,0 +1,13 @@
+package pi2025_semestre6.metro_obras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroObrasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
