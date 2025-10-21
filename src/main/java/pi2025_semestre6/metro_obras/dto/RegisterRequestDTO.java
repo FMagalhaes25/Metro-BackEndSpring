0140f98b687c7c320d1c0c4ba1385006f7212e54
FMagalhaes25 <felipemostaco05@gmail.com>
2025-10-20 22:20:47 -0300

    Refactor: Atualiza DTOs de registro e resposta do usuário com novos campos

--- src/main/java/pi2025_semestre6/metro_obras/dto/RegisterRequestDTO.java
+++ src/main/java/pi2025_semestre6/metro_obras/dto/RegisterRequestDTO.java
@@ -1,4 +1,12 @@
 package pi2025_semestre6.metro_obras.dto;
 
+import pi2025_semestre6.metro_obras.model.UserRole;
+
 // DTO para receber os dados de registro
-public record RegisterRequestDTO(String email, String senha) {}
+public record RegisterRequestDTO(
+        String name,
+        String email,
+        String senha,
+        String cpf,
+        UserRole role // O JSON deve enviar "ADMIN_GLOBAL" ou "FUNCIONARIO"
+) {}
--- src/main/java/pi2025_semestre6/metro_obras/dto/UsuarioResponseDTO.java
+++ src/main/java/pi2025_semestre6/metro_obras/dto/UsuarioResponseDTO.java
@@ -1,9 +1,22 @@
 package pi2025_semestre6.metro_obras.dto;
 
 import pi2025_semestre6.metro_obras.model.Usuario;
+import pi2025_semestre6.metro_obras.model.UserRole;
 
-public record UsuarioResponseDTO(Long id, String email) {
+public record UsuarioResponseDTO(
+        Long id,
+        String name,
+        String email,
+        String cpf,
+        UserRole role
+) {
     public UsuarioResponseDTO(Usuario usuario) {
-        this(usuario.getId(), usuario.getEmail());
+        this(
+                usuario.getId(),
+                usuario.getName(),
+                usuario.getEmail(),
+                usuario.getCpf(),
+                usuario.getRole()
+        );
     }
 }
