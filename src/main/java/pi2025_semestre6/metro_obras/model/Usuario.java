e89af72f43369a24fe93601b61a75dee7cdc9b5b
FMagalhaes25 <felipemostaco05@gmail.com>
2025-10-20 22:20:21 -0300

    Refactor: Expande entidade Usuario com novos campos (name, cpf, role, timestamps)

--- src/main/java/pi2025_semestre6/metro_obras/model/Usuario.java
+++ src/main/java/pi2025_semestre6/metro_obras/model/Usuario.java
@@ -5,10 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.Instant;
 import java.util.Collection;
 import java.util.List;
 
@@ -24,13 +27,44 @@ public class Usuario implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true) //não pode repetir
+    private String name;
+
+    @Column(unique = true, nullable = false) //não pode repetir
     private String email;
+
+    @Column(nullable = false)
     private String senha;
+
+    @Column(unique = true, nullable = false)
+    private String cpf;
+
+    @Enumerated(EnumType.STRING) // Salva a string no banco, ao invés do ID
+    @Column(nullable = false)
+    private UserRole role;
+
+    @CreationTimestamp // Define a data de criação automaticamente
+    @Column(updatable = false)
+    private Instant created_at;
+
+    @UpdateTimestamp // Define a data de atualização automaticamente
+    private Instant updated_at;
+
+    // Construtor customizado para o registro (sem os campos automáticos)
+    public Usuario(String name, String email, String senha, String cpf, UserRole role) {
+        this.name = name;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.role = role;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        if (this.role == UserRole.ADMIN_GLOBAL) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN_GLOBAL"));
+        }
+        // Por padrão, qualquer outro é funcionário
+        return List.of(new SimpleGrantedAuthority("ROLE_FUNCIONARIO"));
     }
 
     @Override
