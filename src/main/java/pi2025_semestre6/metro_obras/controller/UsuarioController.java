8e01593dbced42377bc7f97dd2d621e26909eb67
FMagalhaes25 <felipemostaco05@gmail.com>
2025-11-14 19:10:22 -0300

    feat: novo endpoint para usuario logado (/me)

--- src/main/java/pi2025_semestre6/metro_obras/controller/UsuarioController.java
+++ src/main/java/pi2025_semestre6/metro_obras/controller/UsuarioController.java
@@ -2,10 +2,12 @@ package pi2025_semestre6.metro_obras.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pi2025_semestre6.metro_obras.dto.UsuarioResponseDTO;
+import pi2025_semestre6.metro_obras.model.Usuario;
 import pi2025_semestre6.metro_obras.repository.UsuarioRepository;
 
 import java.util.List;
@@ -26,5 +28,12 @@ public class UsuarioController {
                 .toList();
 
         return ResponseEntity.ok(listaDeUsuariosDTO);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UsuarioResponseDTO> getMe(@AuthenticationPrincipal Usuario usuarioLogado) {
+        // O Spring injeta o 'Usuario' logado automaticamente
+        // Nós apenas o convertemos para o DTO de resposta e retornamos
+        return ResponseEntity.ok(new UsuarioResponseDTO(usuarioLogado));
     }
 }
